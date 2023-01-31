541c80d31fa89d8efe68488822ad1420a11fba0d
Tiago Baptista Noronha <tiago.bn@gmail.com>
2023-01-31 17:57:55 -0300

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Tiago Baptista Noronha on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
